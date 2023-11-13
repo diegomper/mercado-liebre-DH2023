47ce1ec29a519bbfe9dacbeecdec81ab1397f17c
diegomper <dmper9111@gmail.com>
2023-11-13 18:50:12 -0300

    Agregado proyecto Mercado Liebre

---
+++ app.js
@@ -0,0 +1,18 @@
+let express= require("express");
+let app = express();
+let path = require("path");
+
+app.listen(3000, () =>
+    console.log("Servidor corriendo")
+);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+});
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+});
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+});
+app.use(express.static("public"));
